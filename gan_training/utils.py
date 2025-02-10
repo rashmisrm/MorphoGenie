d1169c8e5e0b5816ee9eae1da130f66802bda2a4
rashmisrm <rashmi.srm@gmail.com>
2025-02-10 17:50:51 +0800

    args update

--- gan_training/utils.py
+++ gan_training/utils.py
@@ -227,7 +227,7 @@ class CustomImageFolderCombine3Test(ImageFolder):
             
         return img1, Label1, Label2    
 def return_data(args):
-    name = args.TestDataset
+    name = args.Dataset
     imtype= args.datatype
     dset_dir = args.dset_dir
     
@@ -339,7 +339,7 @@ def return_data(args):
 
 
 def return_data_test(args):
-    name = args.Test_Dataset
+    name = args.Dataset
     imtype= args.datatype
     dset_dir = args.dset_dir
     
